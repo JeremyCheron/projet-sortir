f717739782b9f4ab6e321e43b96a5eb95fa66c42
FannyRoux <fannyrouxfr@gmail.com>
2024-02-26 18:05:39 +0100

    début eventController + EventForm + service Event + Event vue

---
+++ src/Form/EventType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\City;
+use App\Entity\Event;
+use App\Entity\EventStatus;
+use App\Entity\Place;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $duration=[];
+        for($i=30; $i<=2880; $i+=30){
+            $duration[$i]=$i;
+        }
+        $builder
+            ->add('name',TextType::class)
+            ->add('startDate', DateTimeType::class,['html5'=>true, 'widget'=>'single_text'])
+            ->add('registrationDeadline', DateTimeType::class, ['html5'=>true, 'widget'=>'single_text'])
+            ->add('maxRegistrations',IntegerType::class)
+            ->add('duration', ChoiceType::class,['choices'=>$duration])
+            ->add('description', TextareaType::class)
+            ->add('campus', EntityType::class, [
+                'class' => Campus::class,
+                'choice_label' => 'name',
+            ])
+//            ->add('city', EntityType::class,['class'=>Place::class,'choice_label'=>'name'])
+            ->add('place', EntityType::class, [
+                'class' => Place::class,
+                'choice_label' => 'name',
+            ])
+//            ->add('street', EntityType::class,['class'=>Place::class,'choice_label'=>'Street'])
+//            ->add('zipCode', EntityType::class,['class'=>City::class,'label'=>'Zip Code'])
+//            ->add('latitude', EntityType::class,['class'=>Place::class,'label'=>'Latidude'])
+//            ->add('latitude', EntityType::class,['class'=>Place::class,'label'=>'Longidude'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
