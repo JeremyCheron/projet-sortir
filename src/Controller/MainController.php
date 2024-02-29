18ffc7437971a3906919e7c75ccc85b412223bcf
JCheron <jrmcheron@gmail.com>
2024-02-29 15:11:44 +0100

    start translation

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,20 +2,36 @@
 
 namespace App\Controller;
 
+use App\Form\SearchType;
 use App\Repository\EventRepository;
 use App\Services\EventService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/', name: 'main_')]
 class MainController extends AbstractController
 {
-    #[Route('', name: 'home')]
-    public function home(EventService $eventService): Response
+
+    public function __construct(private FormFactoryInterface $formFactory)
     {
+    }
+
+    #[Route('', name: 'home')]
+    public function home(EventService $eventService, Security $security): Response
+    {
+        if (!$security->isGranted('ROLE_USER')) {
+            return new RedirectResponse($this->generateUrl('app_login'));
+        }
+
+        $form = $this->formFactory->create(SearchType::class);
+
         return $this->render('main/home.html.twig', [
-            'events'=>$eventService->getAllEvents()
+            'events'=>$eventService->getAllEvents(),
+            'form' => $form
         ]);
     }
 }
