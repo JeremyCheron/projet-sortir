a8cba6ad0d7a2b9fb5f23ed334681a1ed5080686
JCheron <jrmcheron@gmail.com>
2024-03-02 18:50:09 +0100

    add place filter by city to new event form

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -8,6 +8,7 @@ use App\Entity\Event;
 use App\Entity\EventStatus;
 use App\Entity\Place;
 use App\Entity\User;
+use App\Repository\PlaceRepository;
 use DateTime;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
@@ -19,10 +20,19 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfonycasts\DynamicForms\DependentField;
+use Symfonycasts\DynamicForms\DynamicFormBuilder;
 
 class EventType extends AbstractType
 {
+
+    public function __construct(private readonly PlaceRepository $placeRepository)
+    {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $duration=[];
@@ -31,6 +41,7 @@ class EventType extends AbstractType
         }
         $tomorrow = new DateTime();
         $tomorrow ->modify('+1 day');
+        $builder = new DynamicFormBuilder($builder);
         $builder
             ->add('name',TextType::class)
             ->add('startDate', DateTimeType::class,['html5'=>true,
@@ -44,13 +55,49 @@ class EventType extends AbstractType
             ->add('maxRegistrations',IntegerType::class)
             ->add('duration', ChoiceType::class,['choices'=>$duration])
             ->add('description', TextareaType::class)
+
+            ->add('city', EntityType::class, [
+                'class' => City::class,
+                'choice_label' => 'name',
+                'label' => 'City',
+                'mapped' => false,
+                'placeholder' => 'Select a City'
+            ])
             ->add('place', EntityType::class, [
                 'class' => Place::class,
                 'choice_label' => 'name',
+                'placeholder' => 'Select a city first'
             ])
-            ->add('save', SubmitType::class,['label'=>'Save'])
+
+                        ->add('save', SubmitType::class,['label'=>'Save'])
             ->add('saveAndPublish', SubmitType::class,['label'=>'Publish the event'])
         ;
+
+        $builder->addEventListener(
+            FormEvents::PRE_SUBMIT,
+            function (FormEvent $event) {
+                $form = $event->getForm();
+                $data = $event->getData();
+
+                if (isset($data['city'])) {
+                    $city = $data['city'];
+                    $places = $this->placeRepository->findBy(['city' => $city]);
+
+                    $choices = [];
+                    foreach ($places as $place) {
+                        $choices[$place->getName()] = $place;
+                    }
+
+                    $form->add('place', EntityType::class, [
+                        'class' => Place::class,
+                        'label' => "place",
+                        'choice_label' => 'name',
+                    ]);
+                }
+
+            }
+        );
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
