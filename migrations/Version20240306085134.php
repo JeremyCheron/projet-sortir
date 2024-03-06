6333156f497d565b2fbb58f000ab4e96cdb43094
JCheron <jrmcheron@gmail.com>
2024-03-06 10:58:08 +0100

    update event details

---
+++ migrations/Version20240306085134.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240306085134 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE event ADD image VARCHAR(255) DEFAULT NULL, CHANGE max_registrations max_registrations INT NOT NULL, CHANGE description description LONGTEXT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE event DROP image, CHANGE max_registrations max_registrations INT DEFAULT NULL, CHANGE description description LONGTEXT DEFAULT NULL');
+    }
+}
